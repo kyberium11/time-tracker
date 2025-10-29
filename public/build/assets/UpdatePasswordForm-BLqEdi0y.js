7c674078ad11424f61c14f455efae9275dadef01
kyberium11 <brojeromealtarejos@gmail.com>
2025-10-29 23:16:56 +0800

    build: compile frontend assets for analytics updates

---
+++ public/build/assets/UpdatePasswordForm-BLqEdi0y.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./UpdatePasswordForm.vue_vue_type_script_setup_true_lang-C_wbK4dz.js";import"./app-GduDMQx3.js";import"./TextInput.vue_vue_type_script_setup_true_lang-DHro7C_w.js";import"./PrimaryButton-BWQjCJSY.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
--- public/build/assets/UpdatePasswordForm-CQGZ-FdZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./UpdatePasswordForm.vue_vue_type_script_setup_true_lang-QelugaFN.js";import"./app-Dk8IlJ-Y.js";import"./TextInput.vue_vue_type_script_setup_true_lang-BZYXe6Z2.js";import"./PrimaryButton-DkpSGF3O.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
