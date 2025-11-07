5f18c6c187f550b1ac51edfeb9753ec094fa88b7
kyberium11 <brojeromealtarejos@gmail.com>
2025-11-07 12:30:39 +0800

    Simplify Inertia CSRF handling - let Inertia handle tokens automatically

--- public/build/assets/DeleteUserForm-lKOrNG7H.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DeleteUserForm.vue_vue_type_script_setup_true_lang-C8w12FyU.js";import"./app-B48rezMj.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./TextInput.vue_vue_type_script_setup_true_lang-DpvzzEuh.js";export{o as default};
---
+++ public/build/assets/UpdatePasswordForm-DpNoHayA.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./UpdatePasswordForm.vue_vue_type_script_setup_true_lang-CN9oYzVH.js";import"./app-BHxUvkSS.js";import"./TextInput.vue_vue_type_script_setup_true_lang-HALyrKW_.js";import"./PrimaryButton-DvwbDco-.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
